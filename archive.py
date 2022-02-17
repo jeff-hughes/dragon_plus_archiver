42fd10a665a47deb237983112bf4ceffe9e2fd9c
Jeff Hughes <jeff.hughes@gmail.com>
2022-02-17 16:53:42 -0500

    Connect archive and localize scripts

--- archive.py
+++ archive.py
@@ -2,56 +2,127 @@
 
 import os
 import time
+from typing import List
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Firefox()
-driver.get("https://dnd.dragonmag.com")
+from localize import Localizer
 
-time.sleep(1)
 
-issue_list = driver.find_element(By.CLASS_NAME, "KGDocPicker_editionsList")
-issues = issue_list.find_elements(By.TAG_NAME, "li")
-# for i in issues:
-#     print(i.text)
+def get_issues_list(driver: WebDriver) -> List[WebElement]:
+    """From the main page, get the list of issues."""
+    driver.get("https://dnd.dragonmag.com")
+    time.sleep(1)
 
-first_issue_button = issues[-1].find_element(By.TAG_NAME, "button")
+    issue_list = driver.find_element(By.CLASS_NAME, "KGDocPicker_editionsList")
+    issues = issue_list.find_elements(By.TAG_NAME, "li")
+    # for i in issues:
+    #     print(i.text)
 
-driver.execute_script("arguments[0].scrollIntoView();", first_issue_button)
-visible_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(first_issue_button))
-visible_button.click()
+    return issues
 
-time.sleep(3)
 
-page_list = driver.find_elements(By.CLASS_NAME, "KGDocViewer_page")
+def click_issue_button(driver: WebDriver, issue_elem: WebElement) -> None:
+    """Given an issue, this finds the button and clicks it."""
+    issue_button = issue_elem.find_element(By.TAG_NAME, "button")
+    driver.execute_script("arguments[0].scrollIntoView();", issue_button)
+    visible_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(issue_button))
+    visible_button.click()
 
-# here we want to cycle through each page, and download the content of the iframe;
-# however, the content is only loaded when it is the current or next/previous page, so we need to
-# click the "next" arrow first and let the page load
-arrows = driver.find_element(By.CLASS_NAME, "KGDocViewer_arrows")
-for i, page in enumerate(page_list):
-    iframe = page.find_element(By.TAG_NAME, "iframe")
-    iframe = WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(iframe))
-    # driver.switch_to.frame(iframe)
 
-    with open(f"page{i+1}.html", "w") as f:
-        f.write(driver.page_source)
-    
-    driver.switch_to.parent_frame()
+# def get_page(driver: WebDriver, filename: str = "",
+#              root_dir: str = "", issue_dir: str = "",
+#              common_assets_dir: str = "") -> None:
+#     """Within the context of an iframe, this downloads the content
+#     of the page to a local file."""
+#     localizer = Localizer(root_dir=root_dir, issue_dir=issue_dir, common_assets_dir=common_assets_dir)
+#     converted = localizer.localize_page(driver.page_source, filename)
 
-    try:
-        forward_button = arrows.find_element(By.CLASS_NAME, "KGDocViewer_pages_arrowsNext")
-        forward_button.click()
-        # time.sleep(1)
-    except NoSuchElementException:
-        # we've reached the last page
-        break
+#     os.makedirs(os.path.join(root_dir, issue_dir), exist_ok=True)
+#     with open(os.path.join(root_dir, issue_dir, filename), "w") as f:
+#         f.write(converted)
 
-time.sleep(5)
-driver.close()
+
+def get_all_pages(driver: WebDriver, issue_num: int, outdir: str = "") -> None:
+    """When viewing a particular issue, this downloads all the
+    pages to local files."""
+    page_list = driver.find_elements(By.CLASS_NAME, "KGDocViewer_page")
+
+    all_urls = {}
+    all_pages_source = []
+    issue_dir = f"Issue {str(issue_num).zfill(2)}"
+
+    # here we want to cycle through each page, and download the
+    # content of the iframe; however, the content is only loaded
+    # when it is the current or next/previous page, so we need to
+    # click the "next" arrow first and let the page load
+    arrows = driver.find_element(By.CLASS_NAME, "KGDocViewer_arrows")
+    for i, page in enumerate(page_list):
+        iframe = page.find_element(By.TAG_NAME, "iframe")
+        iframe = WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(iframe))
+        # driver.switch_to.frame(iframe)
+
+        all_pages_source.append(driver.page_source)
+        all_urls[driver.current_url] = i
+
+        # get_page(
+        #     driver,
+        #     filename=f"page{i+1}.html",
+        #     root_dir=outdir,
+        #     issue_dir=issue_dir,
+        #     common_assets_dir="common"
+        # )
+        driver.switch_to.parent_frame()
+
+        try:
+            forward_button = arrows.find_element(By.CLASS_NAME, "KGDocViewer_pages_arrowsNext")
+            forward_button.click()
+            # time.sleep(1)
+        except NoSuchElementException:
+            # we've reached the last page
+            break
+
+    # now that we have all the page sources, we can localize them,
+    # in a context where we know all the URLs for the issue as well
+    for i, source in enumerate(all_pages_source):
+        filename = f"page{i+1}.html"
+        localizer = Localizer(root_dir=outdir, issue_dir=issue_dir, common_assets_dir="common", issue_urls=all_urls)
+        converted = localizer.localize_page(source, filename)
+
+        os.makedirs(os.path.join(outdir, issue_dir), exist_ok=True)
+        with open(os.path.join(outdir, issue_dir, filename), "w") as f:
+            f.write(converted)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Download issues of Dragon+ magazine.")
+    parser.add_argument("-i", "--issue", nargs="+", type=int,
+                        help="Issue numbers to download. Separate the list with spaces. If flag is not set, will download all issues.")
+    parser.add_argument("-o", "--outdir", nargs="?", default="./data",
+                        help="Directory to place output files.")
+    args = parser.parse_args()
+
+    if args.issue is not None:
+        # TODO: handle issue numbers here; need to return back to
+        # main page in between
+        pass
+
+    driver = webdriver.Firefox()
+    issues = get_issues_list(driver)
+
+    click_issue_button(driver, issues[-1])
+    time.sleep(3)  # wait for page to load
+
+    get_all_pages(driver, issue_num=1, outdir=args.outdir)
+
+    driver.close()
--- localize.py
+++ localize.py
@@ -2,16 +2,190 @@
 
 import os
 import re
-from typing import Callable, Optional
+from typing import Callable, Dict, Optional, Tuple
 
 import bs4
 from bs4 import BeautifulSoup
 import requests
 
-STYLES_DIR = "styles"
-FONTS_DIR = "fonts"
-SCRIPTS_DIR = "scripts"
-IMG_DIR = "img"
+
+class Localizer:
+    def __init__(
+            self,
+            issue_urls: Dict[str, int],
+            root_dir: str = "",
+            issue_dir: str = "Issue",
+            common_assets_dir: str = "common") -> None:
+
+        self.issue_urls = issue_urls
+        self.base_url = None
+
+        self.root_dir = root_dir
+        self.issue_dir = os.path.join(root_dir, issue_dir)
+
+        self.common_assets_dir = common_assets_dir
+        self.styles_dir = os.path.join(self.common_assets_dir, "styles")
+        self.fonts_dir = os.path.join(self.common_assets_dir, "fonts")
+        self.scripts_dir = os.path.join(self.common_assets_dir, "scripts")
+        self.images_dir = "img"
+
+        common_to_root = os.path.join(root_dir, common_assets_dir)
+
+        # when making sure the directories exist, now we need to
+        # frame it relative to the current working directory
+        for dr in (root_dir, self.issue_dir, common_to_root, os.path.join(common_to_root, "styles"), os.path.join(common_to_root, "fonts"), os.path.join(common_to_root, "scripts"), os.path.join(self.issue_dir, "img")):
+            os.makedirs(dr, exist_ok=True)
+
+    def localize_page(self, raw_data: str, filename: str) -> str:
+        """Parses a full page and localizes all resources."""
+        soup = BeautifulSoup(raw_data, 'html.parser')
+
+        # remove base href
+        base_href = soup.head.find("base")
+        if base_href is not None:
+            self.base_url = base_href["href"]
+            remove_elem(base_href)
+
+        # download all stylesheets and refer to local copy
+        css = soup.head.find_all("link", rel="stylesheet")
+        for ss in css:
+            ss["href"] = self.localize_item(
+                ss["href"],
+                rel_dir=os.path.join("..", self.styles_dir),
+                abs_dir=os.path.join(self.root_dir, self.styles_dir),
+                formatter=self.localize_css)
+
+        styles = soup.find_all("style")
+        for style in styles:
+            style.string = self.localize_css(style.string, self.base_url, in_subdir=False)
+
+        # delete the manifest and some other metadata
+        remove_elem(soup.head.find("link", rel="manifest"))
+        remove_elem(soup.head.find("meta", attrs={"name": "apple-itunes-app"}))
+
+        og_url = soup.head.find("meta", property="og:url")
+        if og_url is not None:
+            og_url["content"] = os.path.join(self.issue_dir, filename)
+
+        og_image = soup.head.find("meta", property="og:image")
+        if og_image is not None:
+            og_image["content"] = self.localize_item(
+                og_image["content"],
+                rel_dir=self.images_dir,
+                abs_dir=os.path.join(self.issue_dir, self.images_dir),
+                binary=True)
+
+        # remove "web-smart" banner
+        bootstrap = soup.head.find("script", class_="KGPugpigReader-bootstrap")
+        if bootstrap is not None:
+            prev_script = bootstrap.previous_sibling
+            remove_elem(prev_script)
+            remove_elem(bootstrap)
+
+        # download all scripts and refer to local copy
+        js = soup.find_all("script", src=True)
+        for j in js:
+            if "google" in j["src"]:
+                remove_elem(j)
+            else:
+                j["src"] = self.localize_item(
+                    j["src"],
+                    rel_dir=os.path.join("..", self.scripts_dir),
+                    abs_dir=os.path.join(self.root_dir, self.scripts_dir))
+
+        # download all images
+        img = soup.find_all("img")
+        for i in img:
+            # skip SVG elements
+            if not i["src"].startswith("data:"):
+                # remove preload class, which makes all images height
+                # and width of 0
+                if "preload" in i["class"]:
+                    i["class"].remove("preload")
+
+                i["src"] = self.localize_item(
+                    i["src"],
+                    rel_dir=self.images_dir,
+                    abs_dir=os.path.join(self.issue_dir, self.images_dir),
+                    binary=True)
+
+        # TODO: Look through text for links and localize within the
+        # same issue
+
+        return soup.prettify()
+
+    def localize_item(
+            self,
+            url: str,
+            rel_dir: str = ".",
+            abs_dir: str = ".",
+            binary: bool = False,
+            formatter: Optional[Callable[[str, str, Dict[str, str]], str]] = None) -> str:
+        """Takes a URL, downloads the resource locally, and returns
+        the local path where it can be found."""
+        url_parts = url.split("/")
+        rel_path = os.path.join(rel_dir, url_parts[-1])
+        abs_path = os.path.join(abs_dir, url_parts[-1])
+
+        # only re-download if we don't already have the resource
+        if not os.path.exists(abs_path):
+            r = requests.get(url)
+            if r.status_code == 200:
+                if binary:
+                    localized_content = r.content
+                    if formatter is not None:
+                        localized_content = formatter(localized_content, url)
+                    with open(abs_path, "wb") as f:
+                        f.write(localized_content)
+                else:
+                    localized_text = r.text
+                    if formatter is not None:
+                        localized_text = formatter(localized_text, url)
+                    with open(abs_path, "wt") as f:
+                        f.write(localized_text)
+                return rel_path
+            else:
+                print(f"Error downloading file {url}")
+                return url
+        return rel_path
+
+    def localize_one_css_url(self, match_obj: re.Match, orig_url: str, in_subdir: bool = True) -> str:
+        """Given a match object matching a 'url()' CSS expression,
+        this pulls the appropriate resource and returns a localized
+        path."""
+        url = match_obj.group(1)
+        if url.startswith("'data:"):
+            # this is an SVG element
+            return match_obj.group(0)
+        if url.startswith("."):
+            url = url_rel_to_abs(url, orig_url)
+        if url.lower().endswith((".ttf", ".otf", ".woff", ".woff2", ".eot")):
+            rel_dir = os.path.join("..", self.fonts_dir)
+            abs_dir = os.path.join(self.root_dir, self.fonts_dir)
+        else:
+            rel_dir = self.images_dir
+            abs_dir = os.path.join(self.issue_dir, self.images_dir)
+        rel_path = self.localize_item(
+            url,
+            rel_dir=rel_dir,
+            abs_dir=abs_dir,
+            binary=True)
+
+        if in_subdir:
+            return f'url("{os.path.join("..", rel_path)}")'
+        else:
+            return f'url("{rel_path}")'
+
+    def localize_css(self, raw_data: str, orig_url: str, in_subdir: bool = True) -> str:
+        """Searches text for 'url()' and replaces with an absolute
+        URL, based on the URL of the original resource."""
+        return re.sub(r"url\(['\"]?([^'\")]+)['\"]?\)", lambda m: self.localize_one_css_url(m, orig_url, in_subdir), raw_data)
+
+
+def remove_elem(elem: Optional[bs4.element.Tag]):
+    """Removes an element from the BeautifulSoup structure, if it exists."""
+    if elem is not None:
+        elem.decompose()
 
 
 def url_rel_to_abs(relative_url: str, from_url: str) -> str:
@@ -27,120 +201,6 @@ def url_rel_to_abs(relative_url: str, from_url: str) -> str:
 
 
 
-def localize(url: str, local_dir: str = ".", binary: bool = False, formatter: Optional[Callable[[str, str], str]] = None) -> str:
-    """Takes a URL, downloads the resource locally, and returns the
-    local path where it can be found."""
-    url_parts = url.split("/")
-    local_path = os.path.join(local_dir, url_parts[-1])
-    os.makedirs(local_dir, exist_ok=True)
-
-    # only re-download if we don't already have the resource
-    if not os.path.exists(local_path):
-        r = requests.get(url)
-        if r.status_code == 200:
-            if binary:
-                localized_content = r.content
-                if formatter is not None:
-                    localized_content = formatter(localized_content, url)
-                with open(local_path, "wb") as f:
-                    f.write(localized_content)
-            else:
-                localized_text = r.text
-                if formatter is not None:
-                    localized_text = formatter(localized_text, url)
-                with open(local_path, "wt") as f:
-                    f.write(localized_text)
-            return local_path
-        else:
-            print(f"Error downloading file {url}")
-            return url
-    return local_path
-
-
-def localize_one_css_url(match_obj: re.Match, orig_url: str, in_subdir: bool = True) -> str:
-    """Given a match object matching a 'url()' CSS expression, this
-    pulls the appropriate resource and returns a localized path."""
-    url = match_obj.group(1)
-    if url.startswith("'data:"):
-        # this is an SVG element
-        return match_obj.group(0)
-    if url.startswith("."):
-        url = url_rel_to_abs(url, orig_url)
-    if url.lower().endswith((".ttf", ".otf", ".woff", ".woff2", ".eot")):
-        local_dir = FONTS_DIR
-    else:
-        local_dir = IMG_DIR
-    local_path = localize(url, local_dir, binary=True)
-
-    if in_subdir:
-        return f'url("{os.path.join("..", local_path)}")'
-    else:
-        return f'url("{local_path}")'
-
-
-def localize_css(raw_data: str, orig_url: str, in_subdir: bool = True) -> str:
-    """Searches text for 'url()' and replaces with an absolute URL,
-    based on the URL of the original resource."""
-    return re.sub(r"url\(['\"]?([^'\")]+)['\"]?\)", lambda m: localize_one_css_url(m, orig_url, in_subdir), raw_data)
-
-
-def remove_elem(elem: Optional[bs4.element.Tag]):
-    """Removes an element from the BeautifulSoup structure, if it exists."""
-    if elem is not None:
-        elem.decompose()
-
-
-def localize_page(raw_data: str) -> str:
-    base_url = None
-    soup = BeautifulSoup(raw_html, 'html.parser')
-
-    # change base href -- not really critical, but why not
-    base_href = soup.head.find("base")
-    if base_href is not None:
-        base_url = base_href["href"]
-        base_href["href"] = FILENAME
-
-    # download all stylesheets and refer to local copy
-    css = soup.head.find_all("link", rel="stylesheet")
-    for ss in css:
-        ss["href"] = localize(ss["href"], STYLES_DIR, formatter=localize_css)
-
-    styles = soup.find_all("style")
-    for style in styles:
-        style.string = localize_css(style.string, base_url, in_subdir=False)
-
-    # delete the manifest and some other metadata
-    remove_elem(soup.head.find("link", rel="manifest"))
-    remove_elem(soup.head.find("meta", attrs={"name": "apple-itunes-app"}))
-
-    og_url = soup.head.find("meta", property="og:url")
-    if og_url is not None:
-        og_url["content"] = FILENAME
-
-    og_image = soup.head.find("meta", property="og:image")
-    if og_image is not None:
-        og_image["content"] = localize(og_image["content"], IMG_DIR, binary=True)
-
-    # remove "web-smart" banner
-    bootstrap = soup.head.find("script", class_="KGPugpigReader-bootstrap")
-    if bootstrap is not None:
-        prev_script = bootstrap.previous_sibling
-        remove_elem(prev_script)
-        remove_elem(bootstrap)
-
-    # download all scripts and refer to local copy
-    js = soup.find_all("script", src=True)
-    for j in js:
-        j["src"] = localize(j["src"], SCRIPTS_DIR)
-
-    # download all images
-    img = soup.find_all("img")
-    for i in img:
-        i["src"] = localize(i["src"], IMG_DIR, binary=True)
-
-    return soup.prettify()
-
-
 if __name__ == "__main__":
     import argparse
 
@@ -153,7 +213,8 @@ if __name__ == "__main__":
         with open(filepath, "r") as f:
             raw_html = f.read()
 
-        converted = localize_page(raw_html)
+        localizer = Localizer()
+        converted = localizer.localize_page(raw_html, filename=filepath)
 
         file_parts = filepath.split(".")
         file_parts[-2] += "_localized"
